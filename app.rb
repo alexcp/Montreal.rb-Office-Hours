f2310d60cafea39c4501978f404008c706c5467c
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-03-13 19:00:24 -0400

    add an index action

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+
+configure do
+  enable :sessions
+  set :root, File.dirname(__FILE__)
+  set :public_folder, Proc.new { File.join(root, "app/public") }
+  set :views, Proc.new { File.join(root, "app/views") }
+end
+
+get '/' do
+  erb :index
+end
