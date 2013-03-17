987edc319dbd94547d05770b70758a106853fcb0
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-03-17 15:37:37 -0400

    add auth with github

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
+require 'oauth2'
+require 'json'
 require_relative 'app/model/event'
 require_relative 'app/model/google_calendar'
+require_relative 'app/model/attending'
 
 
 configure do
@@ -12,4 +15,36 @@ end
 
 get '/' do
   erb :index
+end
+
+get '/t' do
+  %(<p>Update the <code>#new_client</code> method in the sinatra app and <a href="/auth/github">try to authorize</a>.</p>)
+end
+
+def client
+  OAuth2::Client.new ENV['GITHUB_CLIENT_ID'],
+                     ENV['GITHUB_CLIENT_SECRET'],
+                     site: 'https://github.com',
+                     authorize_url: 'https://github.com/login/oauth/authorize',
+                     token_url: 'https://github.com/login/oauth/access_token'
+end
+
+get '/auth/github' do
+  url = client.auth_code.authorize_url(:redirect_uri => redirect_uri, :scope => 'user')
+  puts "Redirecting to URL: #{url.inspect}"
+  redirect url
+end
+ 
+get '/auth/github/callback' do
+  access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+  user_info = JSON.parse(access_token.get("https://api.github.com/user").body)
+  Attending.new user_info
+  redirect to('/')
+end
+ 
+def redirect_uri(path = '/auth/github/callback', query = nil)
+  uri = URI.parse(request.url)
+  uri.path  = path
+  uri.query = query
+  uri.to_s
 end
