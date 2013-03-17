8589bf523ff911c7dac991d9fe228bf861280b02
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-03-17 12:29:12 -0400

    storing events in a sorted set

--- app/model/event.rb
+++ app/model/event.rb
@@ -1,21 +1,39 @@
 require 'redis'
+require 'time'
 
 class Event
-  attr_reader :uid, :date
+  attr_reader :id, :date
+  ONE_DAY = 86400
 
-  def initialize uid, date
-    @uid = uid 
+  def initialize date
+    @id = Time.parse(date).to_f
     @date = date
     save
   end
 
   def save
-    unless get uid
-      Redis.new.hset("events", uid, date)
-    end
+    Redis.new.zadd(:event, id, date ) unless exist?
   end
 
-  def self.get uid
-    Redis.new.hget 'events', uid 
+  def exist?
+    Redis.new.zscore :event, date
+  end
+
+  def self.current
+    current = all.first
+    remove(current) if has_expired?(current)
+    all.first
+  end
+
+  def self.all
+    Redis.new.zrange(:event, 0, -1)
+  end
+
+  def self.remove(date)
+    Redis.new.zrem :event, date
+  end
+
+  def self.has_expired?(date)
+    Time.parse(date).to_f < Time.now.to_f - ONE_DAY
   end
 end
--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
 
 describe Event do
+  before :each do
+    @event = Event.new "12-03-2013"
+  end
+
   it "should save new event to redis" do
-    event = Event.new "123", "12-03-2013"
-    Event.get("123").should eq("12-03-2013")
+    @event.exist?.should be_true
+  end
+
+  it "should retrieve current event" do
+    Time.stub now: Time.parse("12-02-2013")
+    Event.current.should eq("12-03-2013")
+  end
+
+  it "should update current event when it has past" do
+    event = Event.new "22-03-2013"
+    Time.stub now: Time.parse("14-03-2013")
+    Event.current.should eq("22-03-2013")
+  end
+
+  it "should check if one event has expired" do
+    Time.stub now: Time.parse("14-03-2013")
+    Event.has_expired?("12-03-2013").should be_true
   end
 end
