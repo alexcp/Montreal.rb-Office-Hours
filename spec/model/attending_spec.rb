0fc575425dcd42f72327ca5767c1999dfab25879
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-03-21 10:00:04 -0400

    Stubbed event in attending spec

--- spec/model/attending_spec.rb
+++ spec/model/attending_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Attending do
+  before :each do
+    Event.stub current: "12-12-12"
+  end
+
   it "should save new to a set" do
     info = {"login"=>"alexcp","html_url"=>"","avatar_url"=>""} 
     Attending.add info
